6ae4dc3c25ae1ab933b967c97f566498da069b99
Cristopher Rodriguez <59444688+CristopherMRodriguez@users.noreply.github.com>
2020-12-12 19:08:01 -0500

    added form handling; error notification

--- index.php
+++ index.php
@@ -1,39 +1,59 @@
 <!DOCTYPE html>
+
 <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta HTTP-EQUIV=Refresh CONTENT="4"> -->
     <title>GWP</title>
-    <style type="text/css"> body{
-      text-align: center;
-    }
+    <style type="text/css"> 
+      body{
+        text-align: center;
+      }
+      .red{
+        color: red;
+      }
     </style>
   </head>
+  <?php
+    $errorText = "";
+    $errorEmail = "";
+    $textArea = "";
+    $email = "";
+    
+    if(isset($_POST["submit"])){
+      $textArea = $_POST["text-area"];  
+      $email = $_POST["email"];
+      
+
+      if (empty($textArea)){
+        $errorText = "Please enter a gift message.";
+      } 
+      if(empty($email)){
+        $errorEmail = "Please enter email.";
+      } else {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+          $errorEmail = "Email Must Be a Valid Email Address.";
+        } 
+      }
+    }
+  ?>
   <body onload="getLoc()">
     <h1>GWP QR API</h1>
-    <iframe name="iframe" id="google_map" src=""></iframe></br>
+    <iframe name="iframe" id="google_map" ></iframe></br>
     <form action="index.php" method="POST">
       <h2>Unique Product Number: 123</h2>
       <label>Leave a Gift Message! ❤️</label><br>
-      <textarea class="text-area" name="text-area" rows="4" cols="50"></textarea></br>
+            <textarea class="text-area" name="text-area" rows="4" cols="50"><?php echo htmlspecialchars(textArea); ?></textarea></br>
+      <div class="red"><?php echo $errorText; ?></div>
       </br>
       <label>Please Enter Your Email</label></br>
-      <input name="email" type="text" class="email-input"></br>
+            <input name="email" type="text" class="email-input" value="<?php echo htmlspecialchars(email); ?>"></br>
+      <div class="red"><?php echo $errorEmail; ?></div>
       <br>
       <input name="submit" type="submit" class="submit" value="submit"></br>
     </form>
-    <?php
-    if(isset($_POST["submit"])){
-      echo "</br>
-      <h2>The Server Has Received The Following User Data:</h2></br>";
-      echo "<h3>Email Is: </h3>";
-      echo $_POST["email"];
-      echo "</br>";
-      echo "<h3>Message Is: <span></span></h3>";
-      echo $_POST["text-area"];
-      echo "</br>";
-    } 
-  ?>
+    
     <script>
       var c = function(pos){
         var lat = pos.coords.latitude,
